e4488c034325cfc22e98a31b9a155ae95ca95151
Sparsh2509 <sparshgupta250904@gmail.com>
2025-12-24 00:28:02 +0530

    Making add task ui with proper way

--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def task_list(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+
+    form = TaskForm()
     tasks = Task.objects.all()
+
     return render(request, "tasks/task_list.html", {
+        "form": form,
         "tasks": tasks
     })
