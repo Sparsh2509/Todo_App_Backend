b98616347700cf36e67477a51ad40208946d7a4f
Sparsh2509 <sparshgupta250904@gmail.com>
2025-12-24 14:13:07 +0530

    Update api work done

--- todo_project/tasks/api_urls.py
+++ todo_project/tasks/api_urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .api_views import api_signup , api_login , api_tasks , api_delete_task
+from .api_views import api_signup , api_login , api_tasks , api_delete_task , api_toggle_task , api_update_task
 
 urlpatterns = [
     path("signup/", api_signup),
     path("login/", api_login),
     path("tasks/", api_tasks),
-    path("tasks/<int:task_id>/", api_delete_task)
+    path("tasks/<int:task_id>/delete", api_delete_task),
+    path("tasks/<int:task_id>/toggle/", api_toggle_task),
+    path("tasks/<int:task_id>/update", api_update_task)
+
 
 ]   
--- todo_project/tasks/api_views.py
+++ todo_project/tasks/api_views.py
@@ -144,3 +144,35 @@ def api_toggle_task(request, task_id):
         },
         status=status.HTTP_200_OK
     )
+
+
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Task
+from .serializers import TaskSerializer
+
+
+@api_view(["PATCH"])
+@permission_classes([IsAuthenticated])
+def api_update_task(request, task_id):
+    task = get_object_or_404(
+        Task,
+        id=task_id,
+        user=request.user
+    )
+
+    serializer = TaskSerializer(
+        task,
+        data=request.data,
+        partial=True   # 🔥 allows partial update
+    )
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
