7d720553a481b71bde844dfea1abaeec09180e86
Sparsh2509 <sparshgupta250904@gmail.com>
2025-12-24 17:32:05 +0530

    Fix DRF permission_classes import

--- todo_project/tasks/api_views.py
+++ todo_project/tasks/api_views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view , permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
